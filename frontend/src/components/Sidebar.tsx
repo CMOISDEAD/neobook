2014c843ef3404f013b8a6342822ad2a5bdd8141
Camilo Davila <camilodavina@gmail.com>
2023-09-22 01:24:32 +0000

    feat: sidebar toggle

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
+  RxArrowLeft,
   RxAvatar,
   RxFileText,
   RxGithubLogo,
@@ -10,7 +11,9 @@ import {
 import axios from "axios";
 
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
   const [files, setFiles] = useState([]);
+
   useEffect(() => {
     (async () => {
       const { data } = await axios.get("http://localhost:3000/files");
@@ -18,47 +21,66 @@ export const Sidebar = () => {
     })();
   }, []);
 
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex sticky top-0 flex-col justify-between content-center p-2 w-3/12 h-screen bg-base-200/80">
-      <div className="files">
-        <Link
-          to="/"
-          className="flex gap-4 content-center items-center pb-1 border-b border-primary/50"
-        >
-          <RxHamburgerMenu />
-          <h1 className="text-xl font-bold">Navigation</h1>
-        </Link>
-        <ul className="list-item">
-          {files.map((file, idx) => (
-            <li key={idx} className="my-2">
-              <Link to={`/file/${file}`}>
-                <button
-                  className="flex justify-start w-full btn btn-primary"
-                  value={file}
-                >
-                  <RxFileText />
-                  {file}
-                </button>
-              </Link>
-            </li>
-          ))}
-        </ul>
+    <>
+      <button
+        className={`${
+          isOpen ? "hidden" : "block"
+        } flex fixed top-1 left-1 z-50 p-2 bg-base-300/80`}
+        onClick={handleOpen}
+      >
+        <RxHamburgerMenu />
+      </button>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } flex sticky top-0 flex-col justify-between content-center p-2 w-3/12 h-screen bg-base-200/80`}
+      >
+        <div className="files">
+          <div className="flex gap-4 content-center items-center justify-between pb-1 border-b border-primary/50">
+            <Link to="/" className="flex content-center items-center gap-4">
+              <h1 className="text-xl font-bold">Navigation</h1>
+            </Link>
+            <button className="btn btn-ghost" onClick={handleOpen}>
+              <RxHamburgerMenu />
+            </button>
+          </div>
+          <ul className="list-item">
+            {files.map((file, idx) => (
+              <li key={idx} className="my-2">
+                <Link to={`/file/${file}`}>
+                  <button
+                    className="flex justify-start w-full btn btn-primary"
+                    value={file}
+                  >
+                    <RxFileText />
+                    {file}
+                  </button>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="pt-1 border-t border-primary/50 join">
+          <button className="flex gap-4 content-center items-center w-1/3 btn join-item">
+            <RxAvatar />
+          </button>
+          <button className="flex gap-4 content-center items-center w-1/3 btn join-item">
+            <RxRulerSquare />
+          </button>
+          <a
+            href="https://github.com/CMOISDEAD"
+            target="_blank"
+            className="flex gap-4 content-center items-center w-1/3 btn join-item"
+          >
+            <RxGithubLogo />
+          </a>
+        </div>
       </div>
-      <div className="pt-1 border-t border-primary/50 join">
-        <button className="flex gap-4 content-center items-center w-1/3 btn join-item">
-          <RxAvatar />
-        </button>
-        <button className="flex gap-4 content-center items-center w-1/3 btn join-item">
-          <RxRulerSquare />
-        </button>
-        <a
-          href="https://github.com/CMOISDEAD"
-          target="_blank"
-          className="flex gap-4 content-center items-center w-1/3 btn join-item"
-        >
-          <RxGithubLogo />
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
